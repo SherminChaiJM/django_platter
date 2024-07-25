22573e745aa581596a6b221dae564d3d6cd38767
Shermin Chai <148110549+SherminChaiJM@users.noreply.github.com>
2024-07-26 00:41:26 +0800

    Update decorators.py

--- website/account/decorators.py
+++ website/account/decorators.py
@@ -2,31 +2,6 @@ from django.contrib.auth.decorators import user_passes_test
 from django.core.exceptions import PermissionDenied
 from functools import wraps
 
-def headoffice_required(view_func):
-    def wrapper(request):
-        if request.user.is_authenticated and request.user.is_headoffice:
-            return view_func(request)
-        else:
-            raise PermissionDenied
-    return wrapper
-
-def districtoffice_required(view_func):
-    def wrapper(request):
-        if request.user.is_authenticated and request.user.is_districtoffice:
-            return view_func(request)
-        else:
-            raise PermissionDenied
-    return wrapper
-
-def branchlocation_required(view_func):
-    def wrapper(request):
-        if request.user.is_authenticated and request.user.is_branchlocation:
-            return view_func(request)
-        else:
-            raise PermissionDenied
-    return wrapper
-
-
 def role_required(*roles):
     def decorator(view_func):
         @wraps(view_func)
